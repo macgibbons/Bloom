0bd3d41f7231235fc238228c559217e87da3a063
Mac Gibbons <57641175+macgibbons@users.noreply.github.com>
2020-05-04 12:46:40 -0500

    put and post for Beans

--- Bloom/Controllers/V1/BeansController.cs
+++ Bloom/Controllers/V1/BeansController.cs
@@ -170,5 +170,121 @@ namespace Capstone.Controllers.V1
             }
         }
 
+
+        //----------POST----------
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Bean bean)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO Bean (BeanName, RoastLevel, MASL, RoastDate, Quantity, Rating, TastingNotes, Variety, Process, Notes, Origin, Roaster, UserId, RegionId)
+                        OUTPUT INSERTED.Id
+                        VALUES (@BeanName, @RoastLevel, @MASL, @RoastDate, @Quantity, @Rating, @TastingNotes, @Variety, @Process, @Notes, @Origin, @Roaster, @UserId, @RegionId)";
+
+                    cmd.Parameters.Add(new SqlParameter("@BeanName", bean.BeanName));
+                    cmd.Parameters.Add(new SqlParameter("@RoastLevel", bean.RoastLevel));
+                    cmd.Parameters.Add(new SqlParameter("@MASL", bean.MASL));
+                    cmd.Parameters.Add(new SqlParameter("@RoastDate", bean.RoastDate));
+                    cmd.Parameters.Add(new SqlParameter("@Quantity", bean.Quantity));
+                    cmd.Parameters.Add(new SqlParameter("@Rating", bean.Rating));
+                    cmd.Parameters.Add(new SqlParameter("@TastingNotes", bean.TastingNotes));
+                    cmd.Parameters.Add(new SqlParameter("@Variety", bean.Variety));
+                    cmd.Parameters.Add(new SqlParameter("@Process", bean.Process));
+                    cmd.Parameters.Add(new SqlParameter("@Notes", bean.Notes));
+                    cmd.Parameters.Add(new SqlParameter("@Origin", bean.Origin));
+                    cmd.Parameters.Add(new SqlParameter("@Roaster", bean.Roaster));
+                    cmd.Parameters.Add(new SqlParameter("@UserId", bean.UserId));
+                    cmd.Parameters.Add(new SqlParameter("@RegionId", bean.RegionId));
+
+                    int id = (int)cmd.ExecuteScalar();
+
+                    bean.Id = id;
+                    return CreatedAtRoute("GetBean", new { id = id }, bean);
+                }
+            }
+        }
+
+        ////////----------PUT----------
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromRoute] int id, [FromBody] Bean bean)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"UPDATE Bean
+                                     SET BeanName = @BeanName, RoastLevel = @RoastLevel, MASL = @MASL, RoastDate = @RoastDate, Quantity = @Quantity, 
+                                     Rating = @Rating, TastingNotes = @TastingNotes, Variety = @Variety, Process = @Process, Notes = @Notes, Origin = @Origin,
+                                     Roaster = @Roaster, UserId = @UserId, RegionId = @RegionId 
+                                     WHERE Id = @id";
+
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+                        cmd.Parameters.Add(new SqlParameter("@BeanName", bean.BeanName));
+                        cmd.Parameters.Add(new SqlParameter("@RoastLevel", bean.RoastLevel));
+                        cmd.Parameters.Add(new SqlParameter("@MASL", bean.MASL));
+                        cmd.Parameters.Add(new SqlParameter("@RoastDate", bean.RoastDate));
+                        cmd.Parameters.Add(new SqlParameter("@Quantity", bean.Quantity));
+                        cmd.Parameters.Add(new SqlParameter("@Rating", bean.Rating));
+                        cmd.Parameters.Add(new SqlParameter("@TastingNotes", bean.TastingNotes));
+                        cmd.Parameters.Add(new SqlParameter("@Variety", bean.Variety));
+                        cmd.Parameters.Add(new SqlParameter("@Process", bean.Process));
+                        cmd.Parameters.Add(new SqlParameter("@Notes", bean.Notes));
+                        cmd.Parameters.Add(new SqlParameter("@Origin", bean.Origin));
+                        cmd.Parameters.Add(new SqlParameter("@Roaster", bean.Roaster));
+                        cmd.Parameters.Add(new SqlParameter("@UserId", bean.UserId));
+                        cmd.Parameters.Add(new SqlParameter("@RegionId", bean.RegionId));
+
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!BeanExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+
+
+
+        private bool BeanExists(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, BeanName
+                        FROM Bean
+                        WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    return reader.Read();
+                }
+            }
+        }
     }
 }
