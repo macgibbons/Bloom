4ce583593291cb5ecf282a006051f9bf633e2fa0
Mac Gibbons <57641175+macgibbons@users.noreply.github.com>
2020-05-05 16:27:49 -0500

    user can now add a grinder from the grinder form

--- Bloom/client/src/components/ApplicationViews.js
+++ Bloom/client/src/components/ApplicationViews.js
@@ -7,6 +7,7 @@ import GrinderList from "./equiptment/Grinders/GrinderList";
 import { GrinderProvider } from "./equiptment/Grinders/GrinderProvider";
 import BrewMethodList from "./equiptment/brewMethods/BrewMethodList";
 import { BrewMethodProvider } from "./equiptment/brewMethods/BrewMethodProvider";
+import GrinderForm from "./equiptment/Grinders/GrinderForm";
 
 export default function ApplicationViews() {
   return (
@@ -17,6 +18,10 @@ export default function ApplicationViews() {
           <Route exact path="/" render={() => <Home />} />
           <Route exact path="/Coffee" render={() => <BeanList />} />
           <Route exact path="/Equiptment" render ={() => <GrinderList />} />
+
+          <Route exact path="/Grinders/create" render = {
+                            props => <GrinderForm {...props} />
+                        } />
         </GrinderProvider>
       </BeanProvider>
     </BrewMethodProvider>
