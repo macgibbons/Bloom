897c7b83f78a96255f4889521f34c83d57b0fbaf
Mac Gibbons <57641175+macgibbons@users.noreply.github.com>
2020-05-06 11:51:35 -0500

    now displaying brew list and the brews info

--- Bloom/client/src/components/brews/Brew.js
+++ Bloom/client/src/components/brews/Brew.js
@@ -1,12 +1,27 @@
 import React from "react"
 import { Link } from "react-router-dom"
-
+import moment from 'moment';
 
 export default ({ brew }) => (
     <section className="">
-
         <div className="coffee--card">
-      this is where my brews go
+            
+            <div>
+                <h1>{ brew.brewMethod.method }</h1>
+                <div>{ moment(brew.brewDate).format('MM | DD | YY') }</div>
+            </div>
+
+            <div>
+                <div>{ brew.bean.beanName }</div>
+                <div>{ brew.bean.roaster }</div>
+            </div>
+
+            <div>
+                <div>{ brew.coffeeDose }g</div>
+                <div>{ brew.waterDose }g</div>
+                <div>{ moment.utc(brew.brewTime * 1000).format('m:ss') }</div>
+            </div>
+        
         </div>
 
 
