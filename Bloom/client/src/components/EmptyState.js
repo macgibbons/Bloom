21052b4e889c8f2c2952c6e99a1138dec0f4d223
Mac Gibbons <57641175+macgibbons@users.noreply.github.com>
2020-05-12 12:24:45 -0500

    user can search or filter a brew in the explore page

---
+++ Bloom/client/src/components/EmptyState.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { FaCoffee } from 'react-icons/fa';
+
+const EmptyState = () =>
+
+<div className="empty--state">
+    <FaCoffee size={300} />
+    <div>no results</div>
+</div>
+
+export default EmptyState;
