00d654e01b53ce3f0732fd886c3898efae8b1066
Mac Gibbons <57641175+macgibbons@users.noreply.github.com>
2020-05-12 15:21:49 -0500

    formated comments

--- Bloom/client/src/components/comments/Comment.js
+++ Bloom/client/src/components/comments/Comment.js
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from "react"
 import { Link, Route } from "react-router-dom"
 import { CommentContext } from "./CommentProvider"
 import './Comments.css'
+import { FaRegUserCircle } from "react-icons/fa"
 
 
 export default ({ comment }) => {
@@ -17,11 +18,15 @@ export default ({ comment }) => {
      return(
     <section className="comment">
 
-        <div className="">
-
-        <div className="comment--date">posted: { moment(comment.datePosted).format('MM | DD | YY') }</div>
-        <div>{comment.text}</div>
+        <div className="comment--content">
+            <FaRegUserCircle size={20}/>
+            <div className="comment--text">
+                <div className="comment--date">{comment.user.firstName} {comment.user.lastName}</div>
+                <div>{comment.text}</div>
+            </div>
         </div>
+        
+        <div className="comment--date">{ moment(comment.datePosted).format('MMMM DD YYYY') }</div>
 
 
         
