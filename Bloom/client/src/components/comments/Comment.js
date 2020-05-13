b18f8c3a76fdbdcd3b0e7738cb331c5e5e45f7fb
Mac Gibbons <57641175+macgibbons@users.noreply.github.com>
2020-05-12 21:04:22 -0500

    user can update their own comments

--- Bloom/client/src/components/comments/Comment.js
+++ Bloom/client/src/components/comments/Comment.js
@@ -9,9 +9,14 @@ import { getUser } from "../../API/userManager"
 export default ({ comment }) => {
      // ***** USER *****
     const user = getUser()
-
+    
      // ***** CONTEXT *****
-    const { deleteComment } = useContext(CommentContext)
+    const { deleteComment, updateComment } = useContext(CommentContext)
+
+     // ***** STATE *****
+     const [updatedComment, setUpdatedComment ] = useState(comment.text)
+     const [form, setForm ] = useState(false)
+
 
      // ***** DATA *****
     var moment = require('moment')
@@ -23,37 +28,59 @@ export default ({ comment }) => {
     if(window.confirm(`Are you sure you want to delete this comment`))
         {deleteComment (comment.id)
         }}
-    
-    // ***** COMPONENT *****
-return(
-
-    <section className="comment">
-
-<div className="comment--content">
-
-        <FaRegUserCircle size={20}/>
-        <div className="comment--text">
+    const editComment = () => {
         
-            <div className="comment--user">{user.id === comment.userId ? "" : comment.user.firstName + ": " }</div>
-            <div>{comment.text}</div>
-        </div>
-</div>
-<div className="comment--footer">
+        updateComment({
+            id: comment.id,
+            text: updatedComment,
+            userId: user.id,
+            datePosted: moment().format(),
+            brewId: comment.brewId
+        }).then(()=>{setForm(false)})
+    }
 
-    <div className="comment--controls">
+    // ***** COMPONENT *****
+    return(
+
+    <section className="comment" onClick={ form ? ()=>setForm(false) : "" }>
         {
-            user.id === comment.userId ? 
+            form ?
             <>
-                <div className="comment--control">edit</div>
-                <div>·</div>
-                <div className="comment--control"
-                    onClick={()=>deleteConfirm()}>delete</div>
+            <input className="comment--input" type='text' defaultValue={comment.text}  onChange={evt => setUpdatedComment(evt.target.value)} /> 
+            <button className="submit--btn" type="submit" onClick={evt => {evt.preventDefault(); editComment()}}>submit</button>
+            </> :
+            <>
+                <div className="comment--content">
+                    <FaRegUserCircle size={20}/>
+                    <div className="comment--text">
+                    
+                        <div className="comment--user">{user.id === comment.userId ? "" : comment.user.firstName + ": " }</div>
+                        <div>{comment.text}</div>
+                    </div>
+                </div>
+
+                <div className="comment--footer">
+                    <div className="comment--controls">
+                        {
+                            user.id === comment.userId ? 
+                            <>
+                                <div className="comment--control"
+                                    onClick={()=>setForm(true)}>edit</div>
+                                <div>·</div>
+                                <div className="comment--control"
+                                    onClick={()=>deleteConfirm()}>delete</div>
+                            </>
+                                : 
+                                
+                                <div></div>
+                        }
+                    <div className={user.id === comment.userId ? "userComment--date":"comment--date"} >{timePassed}</div>
+                    </div>
+
+                </div>
+
             </>
-                : <div></div>
-            }
-            <div className={user.id === comment.userId ? "userComment--date":"comment--date"} >{timePassed}</div>
-    </div>
-</div>
+        }
 
 
 
